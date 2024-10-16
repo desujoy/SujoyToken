962f53fab6ebe24477e89a241850bbf8c05f62ef
Sujoy De <sujoy.de345@gmail.com>
2024-10-16 23:02:43 +0530

    Update SujoyToken.sol

--- SujoyToken.sol
+++ SujoyToken.sol
@@ -8,7 +8,7 @@ contract SujoyToken {
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => mapping(address => uint256)) public _allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(
@@ -16,6 +16,7 @@ contract SujoyToken {
         address indexed spender,
         uint256 value
     );
+    event Burn(address indexed burner, uint256 value);
 
     constructor(uint256 _initialSupply) {
         totalSupply = _initialSupply * 10**uint256(decimals);
@@ -38,7 +39,7 @@ contract SujoyToken {
         public
         returns (bool success)
     {
-        allowance[msg.sender][_spender] = _value;
+        _allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
@@ -49,12 +50,31 @@ contract SujoyToken {
         uint256 _value
     ) public returns (bool success) {
         require(balanceOf[_from] >= _value, "Insufficient balance");
-        require(allowance[_from][msg.sender] >= _value, "Allowance exceeded");
+        require(_allowance[_from][msg.sender] >= _value, "Allowance exceeded");
 
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
+        _allowance[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256 remaining)
+    {
+        return _allowance[_owner][_spender];
+    }
+
+    function burn(uint256 _value) public returns (bool success) {
+        require(
+            balanceOf[msg.sender] >= _value,
+            "Insufficient balance to burn"
+        );
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+        emit Burn(msg.sender, _value);
         return true;
     }
 }
